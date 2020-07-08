ffbb76f7cb3c937511ed561d6d82708e361af90a
oohamaka <type_r_bathurst@me.com>
2020-07-09 06:48:43 +0900

    0709 version up

--- index.php
+++ index.php
@@ -1,5 +1,32 @@
+<?php
+
+$old_name = $_FILES['image']['tmp_name'];
+$new_name = $_FILES['image']['name'];
+
+if(!empty($_FILES['image'])){
+$upload = is_uploaded_file('name');
+$move = move_uploaded_file($old_name , 'album/'. $new_name);
+
+var_dump($move);
+$handle = opendir('./album');
+$entry = readdir($handle);
+
+while($entry = readdir($handle)){
+  $images[] = $entry;
+  unlink("..");
+  unlink(".DS_Store");  
+}
+var_dump($images);
+
+closedir($handle);
+}
+
+
+
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="jp">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,35 +37,9 @@
     <input type='file' name='image'>
     <input type='submit' value='送信'>
     </form>
+
+<?php foreach($images as $image): ?>
+    <img src="<?php echo $image; ?> " alt="" srcset="" width="200">
+<?php endforeach ?>
 </body>
 </html>
-
-<?php
-$old_name = $_FILES['image']['tmp_name'];
-$new_name = $_FILES['image']['name'];
-
-var_dump($new_name);
-
-$upload = is_uploaded_file('name');
-$move = move_uploaded_file($old_name,'album/' . $new_name);
-
-var_dump($move);//trueなのでuploadされている
-
-
-$images = array();
-$handle = $opendir('./album');
-$enrtry = readdir($handle);
-$images = $entry;
-
-var_dump($images);
-
-
-closedir($handle);
-
-
-
-//foreach ($images as $img){
-//    echo '<img src="./index.php' . $img .'">';
-//}
-
-?>
